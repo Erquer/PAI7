43770bfb160ea94a675bfbf4bb53851a69b390e9
HP <baryla181@gmail.com>
2020-04-27 13:38:28 +0200

    Poprawka walidacji

--- app/Model/Employee.php
+++ app/Model/Employee.php
@@ -6,12 +6,13 @@ App::uses('AppModel', 'Model');
  */
 class Employee extends AppModel {
 
-	public $validate = array('nazwisko'=> array('rule'=>'notBlank'),
-			'etat'=>array('rule'=>'notBlank'),
-		'placa_pod'=>array('rule'=>'isBetween'));
-
-	public function isBetween($value){
-		return (0<=$value)&&($value<=2000);
-	}
+	public $validate = array(
+		'nazwisko'=> array('rule'=>'notBlank'),
+		'etat'=>array('rule'=>'notBlank'),
+		'placa_pod'=> array(
+        	'rule' => array('range', 0, 2000),
+        	'message' => 'Please enter a number between -1 and 11'
+    	)
+	);
 
 }
